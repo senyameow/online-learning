ed643becb455bb4fc793cb880abbd238679f52ec
senyameow <arseniyxd@gmail.com>
2023-09-11 21:09:06 +0700

    basic modal.tsx file (template)

---
+++ components/ui/modal.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "./dialog";
+
+interface ModalProps {
+    children?: React.ReactNode;
+    title: string;
+    description: string;
+    isOpen: boolean;
+    onClose: () => void;
+}
+
+export const Modal: React.FC<ModalProps> = ({ children, title, description, isOpen, onClose }) => {
+
+    const onChange = (open: boolean) => {
+        if (open) {
+            onClose()
+        }
+    }
+
+    return (
+        <Dialog onOpenChange={onChange} open={isOpen}>
+            <DialogTrigger>Open</DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>{title}</DialogTitle>
+                    <DialogDescription>
+                        {description}
+                    </DialogDescription>
+                </DialogHeader>
+                <div>
+                    {children}
+                </div>
+            </DialogContent>
+        </Dialog>
+    )
+}

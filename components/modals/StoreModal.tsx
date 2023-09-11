d774807d03b8b2cd254793011a5f1dbc21d801eb
senyameow <arseniyxd@gmail.com>
2023-09-11 22:44:03 +0700

    create store modal

--- components/modals/StoreModal.tsx
+++ components/modals/StoreModal.tsx
@@ -3,14 +3,73 @@
 import { useModalStore } from "@/hooks/use-modal-store"
 import { Modal } from "../ui/modal"
 
+import * as z from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({
+    name: z.string().min(1, {
+        message: 'store should have a name'
+    }).max(10, { message: 'wowowowow chill out, big name!' }),
+})
+
 export const StoreModal = () => {
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        // do whatever you want with values from form
+    }
+
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: ''
+        }
+    })
 
     const { onClose, isOpen } = useModalStore()
 
     return (
 
         < Modal title="create a store" description="create a store where you will sell products" isOpen={isOpen} onClose={onClose}>
-            my modal
+            <div>
+                <div className="py-2 pb-4 space-y-4">
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                            <FormField
+                                control={form.control}
+                                name="name"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Store Name</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="shadcn" {...field} className="border border-black ring-0 ring-offset-0 focus-visible:ring-offset-0 focus-visible:ring-0 outline-none " />
+                                        </FormControl>
+
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <div className="pt-6 justify-self-end items-center justify-end place-self-end w-full flex gap-x-4">
+                                <Button variant={'outline'} onClick={onClose}>Cancel</Button>
+                                <Button type="submit" >Submit</Button>
+                            </div>
+                        </form>
+                    </Form>
+                </div>
+            </div>
         </Modal >
     )
 }

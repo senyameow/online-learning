87fe898f6025469a09f64aa7440569eb4edc0b13
senyameow <arseniyxd@gmail.com>
2023-09-18 20:33:53 +0700

    small adjustments in db + stripe

---
+++ app/api/checkout/route.ts
@@ -0,0 +1,84 @@
+import { db } from "@/lib/db"
+import { stripe } from "@/lib/stripe"
+import { NextResponse } from "next/server"
+import Stripe from "stripe"
+
+const corsHeaders = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "POST, GET, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
+
+}
+
+export async function OPTIONS() {
+    return NextResponse.json({}, { headers: corsHeaders })
+}
+
+export default async function POST(req: Request) {
+    try {
+
+        const { searchParams } = new URL(req.url)
+
+        const storeId = searchParams.get('storeId')
+
+        const body = await req.json()
+
+        const { productIds } = body
+
+        if (productIds.length === 0) return new NextResponse('no product ids provided', { status: 400 })
+
+        const purchasedItems = await db.product.findMany({
+            where: {
+                id: {
+                    in: productIds
+                }
+            }
+        })
+
+        const lineItems: Stripe.Checkout.SessionCreateParams.LineItem[] = []
+
+        purchasedItems.forEach(product => {
+            lineItems.push({
+                price_data: {
+                    currency: 'USD',
+                    product_data: {
+                        name: product?.label
+                    },
+                    unit_amount: Number(product.price) * 100
+                },
+                quantity: 1,
+
+            })
+        })
+
+        const order = await db.order.create({
+            data: {
+                storeId: storeId as string,
+                paid: false,
+                OrderItem: {
+                    create: productIds.map((id: string) => ({
+                        Product: {
+                            connect: {
+                                id
+                            }
+                        }
+                    }))
+                }
+
+            }
+        })
+
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ["card"],
+            line_items: lineItems,
+            mode: 'payment',
+            success_url: ``,
+            cancel_url: ``
+        })
+
+
+
+    } catch (error) {
+        console.log(error)
+    }
+}

0d76475a0155318d61933cf4c7b6f98621d896a3
senyameow <arseniyxd@gmail.com>
2023-09-18 21:33:46 +0700

    checkout

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -7,7 +7,6 @@ const corsHeaders = {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "POST, GET, PUT, DELETE, OPTIONS",
     "Access-Control-Allow-Headers": "Content-Type, Authorization",
-
 }
 
 export async function OPTIONS() {
@@ -18,11 +17,9 @@ export default async function POST(req: Request) {
     try {
 
         const { searchParams } = new URL(req.url)
-
         const storeId = searchParams.get('storeId')
 
         const body = await req.json()
-
         const { productIds } = body
 
         if (productIds.length === 0) return new NextResponse('no product ids provided', { status: 400 })
@@ -54,7 +51,6 @@ export default async function POST(req: Request) {
         const order = await db.order.create({
             data: {
                 storeId: storeId as string,
-                paid: false,
                 OrderItem: {
                     create: productIds.map((id: string) => ({
                         Product: {
@@ -64,7 +60,6 @@ export default async function POST(req: Request) {
                         }
                     }))
                 }
-
             }
         })
 
@@ -72,11 +67,20 @@ export default async function POST(req: Request) {
             payment_method_types: ["card"],
             line_items: lineItems,
             mode: 'payment',
-            success_url: ``,
-            cancel_url: ``
+            success_url: `${process.env.FRONTEND_STORE_URL}/cart?success=123`,
+            cancel_url: `${process.env.FRONTEND_STORE_URL}/cart?canceled=123`,
+            phone_number_collection: {
+                enabled: true
+            },
+            billing_address_collection: 'required',
+            metadata: {
+                orderId: order.id
+            }
         })
 
-
+        return NextResponse.json({ url: session.url }, {
+            headers: corsHeaders
+        })
 
     } catch (error) {
         console.log(error)

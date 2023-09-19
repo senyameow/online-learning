a19ebd3ad9b09b6e9a929c112ea388171dadf436
senyameow <arseniyxd@gmail.com>
2023-09-20 01:38:46 +0700

    middleware change clerk

--- app/(dashboard)/_components/SidebarRoute.tsx
+++ app/(dashboard)/_components/SidebarRoute.tsx
@@ -14,7 +14,7 @@ const SidebarRoute = ({ href, title, active, icon }: SidebarRouteProps) => {
     const Icon = icon
 
     return (
-        <Link href={href} className={cn(`w-full hover:text-blue-100/80`, active && 'bg-blue-200 border-r border-[2.3px] border-blue-700 text-blue-500')}>
+        <Link href={href} className={cn(`w-full hover:bg-blue-100/80`, active && 'bg-blue-200 border-r border-[2.3px] border-blue-700 text-blue-500')}>
             <div className='p-6 flex flex-row justify-start items-center'>
                 <Icon className={'w-6 h-6 mr-2'} />
                 <span className='text-muted-forehead text-[20px]'>{title}</span>

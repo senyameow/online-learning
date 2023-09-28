a2e2b734cb93c5b74e231651b4c182c5d8fd792b
senyameow <arseniyxd@gmail.com>
2023-09-28 19:38:02 +0700

    cardOverview + graph

--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const page = () => {
+const Analitics = () => {
     return (
-        <div>Analytics</div>
+        <div className=''>
+
+        </div>
     )
 }
 
-export default page
+export default Analitics
